cf8259c21238297fade27e78b95031871dbc4d99
andrew lorien <subscriptions@andrewswebsite.net>
2019-01-22 00:17:17 +1100

    Is Andrew At The Bowlo site : create-new-app

---
+++ src/entry.js
@@ -0,0 +1,24 @@
+// You may or may not need this depending on
+// what JavaScript features you're using - e.x. async / await.
+// Feel free to remove it and see what happens!
+import '@babel/polyfill'
+
+// Import our top-level sass file.
+import './styles/styles.scss'
+
+// Import React.
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import our top-level component.
+import App from 'components/App'
+
+// Create a single element for our app to live.
+document.body.innerHTML = '<div id="app"></div>'
+document.body.className = 'bg-black-80 fw4 white-80'
+
+// Mount our app.
+ReactDOM.render(
+  <App />,
+  document.querySelector('#app')
+)
