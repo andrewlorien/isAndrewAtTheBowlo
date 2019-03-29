0f5dcddb38f4c70e2da255ee45baa7ce948de7e0
Andrew Lorien <andrew.lorien@complispace.com.au>
2019-03-29 16:37:45 +1100

    update readme and function

--- isAndrewAtTheBowlo_lambda/README.md
+++ isAndrewAtTheBowlo_lambda/README.md
@@ -3,3 +3,5 @@
 # AWS API/Lambda endpoint
 - API endpoint with a secret key which i'll have to add as a parameter to the app
 - Lambda function which writes a name+time json request to ishe.json
+
+The .py code in this folder is not actually deployed.  It should be identical to the code in cloudformation/isAndrewAtTheBowlo_lambdaGateway.yaml, but if they do not match the cloudformation code is what i've deployed
--- isAndrewAtTheBowlo_lambda/isAndrewAtTheBowlo_lambda.py
+++ isAndrewAtTheBowlo_lambda/isAndrewAtTheBowlo_lambda.py
@@ -2,37 +2,40 @@ import boto3
 import json
 import datetime
 
+
 AWS_BUCKET_NAME = 'isandrewatthebowlo.com'
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(AWS_BUCKET_NAME)
-false=False
 
 def test():
     when = datetime.datetime.now().isoformat()
     whatDoesHeSay = "soon"
-    jsondata = '{"ishe":"' + whatDoesHeSay + '","when":"' + when + '"}'
+    jsondata = '{"ishe":"' + whatDoesHeSay + '","when":"' + when + 'Z"}'
     return jsondata
 
     
 def debuggit(event, context):
     path = 'debuggit.json'
     when = datetime.datetime.now().isoformat()
-    print("Received event: " + json.dumps(event, indent=2))
-    jsondata = str(event['queryStringParameters'])      
-    body = putitinthebucket(jsondata,path)
-    return response(body)
+    jsondata = str(event)      
+    putitinthebucket(jsondata,path)
     
 # call this function with a PUT request containing a json object like {"whatDoesHeSay": "no"}
 def telltheworld(event, context):
     path = 'ishe.json'
     when = datetime.datetime.now().isoformat()
-    whatDoesHeSay = event['queryStringParameters']['whatDoesHeSay']
-    jsondata = '{"ishe":"' + whatDoesHeSay + '","when":"' + when + '"}'  
+    querystring = event['queryStringParameters']
+    whatDoesHeSay = querystring['whatDoesHeSay']
+    jsondata = '{"ishe":"' + whatDoesHeSay + '","when":"' + when + 'Z"}'
     body = putitinthebucket(jsondata,path)
-    # now send an SMS to everybody on the SQS topix list
     
-    return response(body)
-
+    print (body) 
+    return {
+        "isBase64Encoded": False,
+        "headers": { "Content-Type": "application/json" },
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
 #{
 #"ishe":"no",
 #"when":"2019-01-08Z23:00:00"
@@ -43,7 +46,7 @@ def putitinthebucket(jsondata,path):
         ACL='public-read',
         ContentType='application/json',
         Key=path,
-        Body=jsondata
+        Body=jsondata,
     )
 
     body = {
@@ -51,15 +54,4 @@ def putitinthebucket(jsondata,path):
         "bucket": AWS_BUCKET_NAME,
         "path": path
     }
-    print ("about to return this body")
-    print (body)
     return body
-
-def response(body):
-    return {
-        "statusCode": 200, \
-        "isBase64Encoded": false, \
-        "headers": {"content-type" : "application/json","x-andrew-test" : "wedontknow"}, \
-        "body": json.dumps(body) \
-    }
-
