15ef3d18268ec70ec705cf4b1e855bd11b7750d5
andrew lorien <subscriptions@andrewswebsite.net>
2023-04-15 16:02:38 +1000

    bowlo fonts (and prep for background images)

--- isandrewatthebowlo_site/src/components/App.jsx
+++ isandrewatthebowlo_site/src/components/App.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const App = () => (
   <Fragment>
     <header className='pv5 bg-bowloboard black-80'>
-      <h1 className='mt0 mb1 tc'>Is Andrew at the Bowlo?  #863f04</h1>
+      <h1 className='mt0 mb1 tc'>Is Andrew at the Bowlo?</h1>
       
     </header>
     <h2 id='isHeThere' className='pt4 f-10rem pb1 tc'>I don't actually know</h2>
